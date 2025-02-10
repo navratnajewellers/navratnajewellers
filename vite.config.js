6f81db88c6364233c0331814e77649e2e0baf69a
Navratna Jewellers <navratnajewellers0@gmail.com>
2025-02-10 17:17:24 +0530

    add rollup-plugin-visualizer package to visualize the build size, add manual code split

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,42 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }), // Opens a bundle analysis report
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('lodash')) {
+              return 'lodash'; // Moves lodash to its own chunk
+            }
+            if (id.includes('date-fns') || id.includes('moment')) {
+              return 'utilities'; // Groups date libraries into one chunk
+            }
+            if (id.includes('rsuite')) {
+              return 'rsuite'; // Separate chunk for rsuite module
+            }
+            if (id.includes('motion')) {
+              return 'motion'; // Separate chunk for rsuite module
+            }
+            if (id.includes('crypto-js')) {
+              return 'crypto-js'; // Separate chunk for rsuite module
+            }
+            if (id.includes('axios')) {
+              return 'axios'; // Separate chunk for rsuite module
+            }
+            return 'vendor'; // Other node_modules go to the vendor chunk
+          }
+        },
+      },
+    },
+  },
   // build: {
   //   rollupOptions: {
   //     output: {
